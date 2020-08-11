8958c8c29f9083f6c8a65f0da324a80eedcaf130
Tommy Moawad <tommymoawad@Tommys-MBP.attlocal.net>
2020-08-11 10:49:26 -0400

    Juypter notebook is better

--- bible-verse-auto-populate/src/App.js
+++ bible-verse-auto-populate/src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import logo from "./logo.svg";
 import "./App.css";
 import MyEditor from "./MyEditor";
 
+async function scrape() {
+  await axios.get(
+    "https://medium.com/@masnun/handling-timeout-in-axios-479269d83c68"
+  );
+}
+
 function App() {
+  const [number, setNumber] = useState(0);
+
+  useEffect(scrape());
+
   return (
     <div className="App">
-      <body className="App-body">Demo</body>
-      <MyEditor />
+      <body className="App-body">Your number is </body>
+
+      <input value={number} onChange={(e) => setNumber(e.target.value)} />
+      {/* <MyEditor /> */}
     </div>
   );
 }
